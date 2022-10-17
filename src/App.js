34cf2d4700afba7836e5378da3ca646d3e5752c8
Kokate0307Rutuja <rutujakokate2000@gmail>
2022-10-17 12:55:01 +0530

    complete basic

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import ClassComponent from "./pages/ClassComponent";
+import FunctionalComponent from "./pages/FunctionalComponent";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+// HOC layout
+
+function App() {
+    const company = "Devtown";
+
+    const [name, setName] = useState("Aditya");
+
+    return ( <
+        Routes >
+        <
+        Route path = "/"
+        element = { < ClassComponent / > }
+        />{" "} <
+        Route path = "/functional-component"
+        element = { <
+            FunctionalComponent
+            name = { name }
+            age = { 20 }
+            company = { company }
+            setName = { setName }
+            />
+        }
+        />{" "} <
+        /Routes>
+    );
+}
+
+export default App;
